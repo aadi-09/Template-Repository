7817d3f79c98f33d16d785f51fd81da0c4e4ab2f
Aaditya Dubey <ad2340033@gmail.com>
2025-02-12 13:21:57 +0530

    Create Component.jsx

---
+++ components/Component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div>
+            <h2>Example Component</h2>
+            <p>This is a basic React component inside the components folder.</p>
+        </div>
+    );
+};
+
+export default Component;
